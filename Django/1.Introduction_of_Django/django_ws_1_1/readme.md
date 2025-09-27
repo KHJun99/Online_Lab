c9dee664d64e0946a28556c6dd6eae11752a506a
KHJun99 <wnsk0524@naver.com>
2025-09-27 12:40:19 +0900

    Remove old dotted directories; keep underscore & 3_Model layout

--- Django/1.Introduction_of_Django/django_ws_1_1/readme.md
+++
@@ -1,5 +0,0 @@
-python -m venv
-source venv/scripts/activate
-pip install -r requirements
-cd new_pjt
-python manage.py runserver
--- Django/2.Templates/django_hw_2_4/my_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from my_app import views
-
-app_name = 'introduce'
-urlpatterns = [
-    path('<str:username>', views.introduce, name='introduce')
-    
-]
